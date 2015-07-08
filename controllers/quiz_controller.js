de7cbdb470ca30ddbb1beea178d802c212d44daa
Pedro Gasané <pgasane@gmail.com>
2015-07-08 14:27:44 +0100

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,21 @@
+// Cargamos el Modelo del MVC para poder acceder a la DB
+// models.js cargará, al ser invocado, la BD Quiz
+var models = require('../models/models.js');
+
 //GET /quizes/question
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res) {
-	if (req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	models.Quiz.findAll().success(function(quiz) {
+		if (req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		} else {
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}		
+	})
 };
---
+++ models/models.js
@@ -0,0 +1,49 @@
+// Se manejan rutas por lo que necesitamos cargar path
+var path = require('path');
+
+// Postgress DATABASE_URL = postgress://user:passwd@host:port/database
+// sqlite    DATABASE_URL = sqlite://://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user	 = (url[2]||null);
+var pwd		 = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect	 = (url[1]||null);
+var port 	 = (url[5]||null);
+var host	 = (url[4]||null);
+var storage	 = process.env.DATABASE_STORAGE;
+
+// models.js construye la DB y el modelo a partir de lo indicado en quiz.js
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar DB SQLite o Postgress
+var sequelize = new Sequelize(DB_name, user, pwd, 
+	{ dialect:  protocol,
+	  protocol: protocol,
+	  port:     port,
+	  host:     host,
+	  storage:  storage, // solo en SQLite (.env)
+	  omitNull: true     // solo en Postgres
+	}
+);
+
+
+// Importar la definición de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Exportar la definición de la tabla Quiz para que sea posible accedar a
+// la tabla desde otras partes de la aplicación con el import correspondiente
+exports.Quiz = Quiz;
+
+// Inicializar la tabla de preguntas en la DB solo si está vacía
+// success() ejecuta el manejador una vez creada la tabla
+sequelize.sync().success(function(){
+	Quiz.count().success(function(count){
+		if (count === 0) {
+			Quiz.create({pregunta: 'Capital de Italia', respuesta: 'Roma'})
+			.success(function(){console.log('Base de datos inicializada.')});
+		};
+	});
+});
