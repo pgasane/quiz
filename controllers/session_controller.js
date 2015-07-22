36154b09da11e81a1acb7ff324f22551c4677135
Pedro Gasané <pgasane@gmail.com>
2015-07-22 09:24:57 +0100

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }      
+};
+
 // GET /login  -- Formulario de login
 exports.new = function(req, res) {
   // Se crea y/o inicializa la variable de errores de sesión
